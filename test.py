2585691976d86bb478f99acdf104c475009a5856
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-08 11:06:09 +0530

    python coding day 1

---
+++ test.py
@@ -0,0 +1,123 @@
+#print ( 2 < 4 )
+
+#min_score = 13
+#score = 13
+
+#print(score > min_score)
+#print(score <= min_score)
+
+#'python'>'Python'
+
+#x = -10
+#if x < 0:
+#    print("The negative number ",  x, " is not valid here.")
+#print("This is always printed")
+
+#if 4 + 5 == 10:
+#    print("TRUE")
+#else:
+#    print("FALSE")
+#print("TRUE")
+
+
+# 1st if statement
+#if True: print('hello')
+
+# 2nd if statement
+#if (5,10):
+#    print('hello')
+
+# 3rd if statement
+#if (yes):
+#    print('hello')
+
+# 4th if statement
+#if (5,10): print('hello')
+
+# x = 0
+# a = 6
+# b = 6
+# if a > 0:
+#     if b < 0: 
+#         x = x + 6 
+#     elif a > 6:
+#         x = x + 5
+#     else:
+#         x = x + 4
+# else:
+#     x = x + 3
+
+# print(x)
+
+# num_1 = int(input("enter first number = "))
+# num_2 = int(input("Enter second number = "))
+
+# total = num_1 + num_2
+
+# print("the total is = ", total)
+
+# for letter in "python":
+#     print(letter)
+
+# import pyjokes
+
+# jokes = pyjokes.get_joke()
+
+# print(jokes)
+
+# import math
+
+# for i in range(1, 11):
+#     i = i * 5
+#     print(i)
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# engine.say("""
+# I met an old woman at the bus stop today.
+# She talked to me, but I didn’t know what to say.
+# ‘Your little brother looks smart,’ she said.
+# But I didn’t know what to say.
+
+# ‘I’m from Romania,’
+# ‘I’ve been here six years,’
+# ‘I sleep in the shelters.”
+# I held my brother’s hand tighter.""")
+# engine.runAndWait()
+
+
+import os
+
+# print(os.listdir('.'))
+
+# print(os.getcwd())
+
+# print(os.open('test.py', os.O_RDONLY))
+
+
+# directory_path = "/"
+
+# contents = os.listdir(directory_path)
+
+# for item in contents:
+#     print(item)
+
+# a = "This is the first line \nthis is the second line"
+# print(a)
+
+# name = input("Enter your name: ")
+
+# print(f"Good Afternoon {name}")
+
+# letter = """Dear <|Name|>,
+#         You are selected!
+#         <|Date|>"""
+
+# print(letter.replace("<|Name|>", "Anirban").replace("<|Date|>", '13th sept, 1996'))
+
+# name = "harry  isagoodboyandagreatabuddy"
+
+# if (name.find("goo")):
+#     print(name.index("goo"))
+
