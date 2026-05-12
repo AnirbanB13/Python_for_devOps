b286006eadeb4b2cde91fbc1beb869f88b67a7b7
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-12 10:38:45 +0530

    Day 4 Learning

--- push.sh
+++ push.sh
@@ -2,16 +2,16 @@
 
 "This script automates the process of adding, committing, and pushing changes to a Git repository."
 
-"***Status***"
+"######Status######"
 git status
 
-"***Adding changes to staging area***"
+"######Adding changes to staging area######"
 git add .
 
-"***Committing changes***"
+"######Committing changes######"
 read -p "Enter commit message: " commit_message
 
 git commit -m "$commit_message"
 
-"***Pushing changes to remote repository***"
+"######Pushing changes to remote repository######"
 git push origin master
