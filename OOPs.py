ba38e4c0472ac39b244043e1662073957e52dd6e
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-26 00:08:04 +0530

    test

--- OOPs.py
+++ OOPs.py
@@ -1,19 +1,20 @@
-# Objective oriented Programming #
+# # Objective oriented Programming #
 
-# class is a blueprint of objects
-# object is an instance of a class
+# # class is a blueprint of objects
+# # object is an instance of a class
 
-class Employee:
-    name = "harry"
-    language = "python"   #class attribute
-    salary = 5000  
+# class Employee:
+#     name = "harry"
+#     language = "python"   #class attribute
+#     salary = 5000  
 
-    def getInfo(self):
-        print(f"the language is {self.language} and salary is {self.salary}")
+#     def getInfo(self):
+#         print(f"the language is {self.language} and salary is {self.salary}")
 
-harry = Employee()
-# harry.lastname = "singh"  #instance attribute
-# print(harry.name, harry.language)
+# harry = Employee()
+# # harry.lastname = "singh"  #instance attribute
+# # print(harry.name, harry.language)
 
-#instance attribute takes precedence over class attribute duing assignment and retrieval
-Employee.getInfo(harry)
+# #instance attribute takes precedence over class attribute duing assignment and retrieval
+# Employee.getInfo(harry)
+
