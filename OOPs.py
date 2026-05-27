305e675d6418fc9e7f3f5b6e73fa19503d7932ce
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-27 07:16:22 +0530

    refactor: comment out all existing code in OOPs.py

--- OOPs.py
+++ OOPs.py
@@ -3,34 +3,34 @@
 # # class is a blueprint of objects
 # # object is an instance of a class
 
-class Employee:
-    name = "harry"
-    language = "python"   #class attribute
-    salary = 5000  
+# class Employee:
+#     name = "harry"
+#     language = "python"   #class attribute
+#     salary = 5000  
 
-    def __init__(self, name, language, salary):  #dunder method which is automatically called
-        print("I am creating an object")
-        self.name = name
-        self.language = language
-        self.salary = salary
+#     def __init__(self, name, language, salary):  #dunder method which is automatically called
+#         print("I am creating an object")
+#         self.name = name
+#         self.language = language
+#         self.salary = salary
 
-#   def getInfo(self):
-#       print(f"the language is {self.language} and salary is {self.salary}")
+# #   def getInfo(self):
+# #       print(f"the language is {self.language} and salary is {self.salary}")
     
-    @staticmethod
-    def greet():
-        print("hello world")
+#     @staticmethod
+#     def greet():
+#         print("hello world")
 
-# harry = Employee()
-# harry.lastname = "singh"  #instance attribute
-# print(harry.name, harry.language)
+# # harry = Employee()
+# # harry.lastname = "singh"  #instance attribute
+# # print(harry.name, harry.language)
 
-# #instance attribute takes precedence over class attribute duing assignment and retrieval
-# Employee.getInfo(harry)
+# # #instance attribute takes precedence over class attribute duing assignment and retrieval
+# # Employee.getInfo(harry)
 
-# self is neccessary to provide in methods within class
+# # self is neccessary to provide in methods within class
 
-harry = Employee("harry", "javascript", 10000)
-harry.greet()
-print(harry.name, harry.language, harry.salary)
+# harry = Employee("harry", "javascript", 10000)
+# harry.greet()
+# print(harry.name, harry.language, harry.salary)
 
