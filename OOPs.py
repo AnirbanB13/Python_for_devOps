c3cf0fa987632f94c62670d0199b9f1d502fbffb
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2026-05-27 15:11:45 +0530

    Day 13 learning: Practice set

--- OOPs.py
+++ OOPs.py
@@ -34,3 +34,37 @@
 # harry.greet()
 # print(harry.name, harry.language, harry.salary)
 
+# class Programmer:
+#     company = "Microsoft"
+
+#     def __init__(self, name, salary, pin):
+#         self.name = name
+#         self.salary = salary
+#         self.pin = pin
+        
+#     def getinfo(self):
+#         print(f"The name of the programmer is {self.name}, salary is {self.salary} and pin is {self.pin}")
+
+
+# p = Programmer("anirban", 100000, 797979)
+
+# # print(p.name, p.salary, p.pin, p.company)
+
+# p.getinfo()
+
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+    def squareRoot(self):
+        print(f"The square root is {self.n**(1/2)}")
+    
+
+a = Calculator(4)
+a.square()
+a.cube()
+a.squareRoot()
